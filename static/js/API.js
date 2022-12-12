c14556f1e530f14e2cc2cf8fadc1f26a4b79f174
Andres <brinezlopez08@gmail.com>
2022-12-12 15:39:17 -0500

    Hacer  conexion con API

---
+++ static/js/API.js
@@ -0,0 +1,31 @@
+
+export const getInformationApi = async (name) => {
+    
+    let api= ""
+    if (name==undefined){ // si no pasa parametros
+        api = `https://restcountries.com/v2/all`
+    }
+    else{
+        api = `https://restcountries.com/v2/name/${name}`
+    }
+
+    const response = await fetch(api)
+    const data = await response.json()
+
+    
+
+    return data
+}
+
+// getInformation('colombia').then (data => console.log(data))
+
+
+
+
+
+
+
+
+
+
+
