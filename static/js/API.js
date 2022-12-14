8b471af62b9d98a7d201079d4d7b76f09b31ebc4
Andres <brinezlopez08@gmail.com>
2022-12-13 21:47:01 -0500

    cambios en las  líneas

--- static/js/API.js
+++ static/js/API.js
@@ -1,13 +1,14 @@
-
-export const getInformationApi = async (name) => {
-    
+export const getInformationApi = async (name,query) => {
     let api= ""
+    
     if (name==undefined){ // si no pasa parametros
         api = `https://restcountries.com/v2/all`
     }
     else{
-        api = `https://restcountries.com/v2/name/${name}`
+        api = `https://restcountries.com/v2/${query}/${name}`
     }
+
+    
 
     const response = await fetch(api)
     const data = await response.json()
